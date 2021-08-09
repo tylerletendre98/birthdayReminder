1db4adf3b0eb675f1c2c9e000a1a15851ce0a8e4
T U <tylerletendre98@gmail.com>
2021-08-09 10:10:32 -0400

    added in the data file for the birthday reminders

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import data from "./data";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Reminder project setup</h1>
+    </div>
+  );
+}
+
+export default App;
